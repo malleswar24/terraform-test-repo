68507592b6d7dbd475d2ef0c6ea131b717d893f2
malleswar24 <56032795+malleswar24@users.noreply.github.com>
2023-07-05 09:34:54 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "malli-dev-tf-statefile"
     key = "main"
-    region = "us-east-1"
+    region = "eu-west-1"
     dynamodb_table = "dev-tf-statefile"
   }
 }
