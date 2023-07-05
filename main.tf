060f491d992dae60a8d513e6479bcab61badb9b6
malleswar24 <56032795+malleswar24@users.noreply.github.com>
2023-07-05 09:38:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,4 @@ resource "aws_instance" "myFirstInstance" {
   ami           = "ami-05432c5a0f7b1bfd0"
   key_name = "malli-dev"
   instance_type = "t2.micro"
-  tags= {
-    Name = Test server
-  }
 }
