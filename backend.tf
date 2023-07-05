ce5751f099a53ad7560758c2b75a0da9213e9b95
malleswar24 <56032795+malleswar24@users.noreply.github.com>
2023-07-05 09:32:36 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "malli-dev-tf-statefile"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "dev-tf-statefile"
   }
 }
